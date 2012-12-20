b57006a57570054057290d58e4101415bc6967ee
Saadullah Saeed <saadullah.saeed@gmail.com>
2012-12-19 19:55:13 -0800

    covering more specs

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,7 +2,6 @@ class Report < ActiveRecord::Base
   belongs_to :report
   belongs_to :datasource
   belongs_to :user
-  belongs_to :project
   
   attr_accessible :config, :filters, :description, :datasource_id, :title, :report_type, :user_id, :project_id, :cache_time
   
