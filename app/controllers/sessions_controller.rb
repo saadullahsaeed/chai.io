a554b194c23bcbc946daa95bd3087077b30e5de9
Saadullah Saeed <saadullah.saeed@gmail.com>
2013-03-05 23:28:30 -0800

    post sign in redirect to projects page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to "/reports", :notice => "Logged in!"
+      redirect_to "/projects", :notice => "Logged in!"
     else
       flash.now.alert = "Invalid email or password"
       render "home/index"
